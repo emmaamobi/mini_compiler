0d150f5aeb1b4c6e602f8c4784d0f00e53dcefaa
Edward Hubbard <edhubbard1994@gmail.com>
2019-11-06 20:48:06 -0600

    copy cases from toFormatted

--- src/main/scala/behaviors.scala
+++ src/main/scala/behaviors.scala
@@ -59,9 +59,35 @@ object behaviors {
   }
   def toFormattedString(e: Expr): String = toFormattedString("")(e)
 
-  def toPrettyFormat(e: Expr): String = {
+  def toPrettyFormat(e: Expr): String = e match {
     case Constant(c) => c.toString
     case UMinus(r) => buildUnaryExprString(r.toString,"--",toFormattedString(INDENT)(r))
+    case Plus(l, r)  => buildExprString("", "Plus", toFormattedString( INDENT)(l), toFormattedString(INDENT)(r))
+    case Minus(l, r) => buildExprString("", "Minus", toFormattedString(INDENT)(l), toFormattedString(INDENT)(r))
+    case Times(l, r) => buildExprString("", "Times", toFormattedString(INDENT)(l), toFormattedString(INDENT)(r))
+    case Div(l, r)   => buildExprString("", "Div", toFormattedString(INDENT)(l), toFormattedString(INDENT)(r))
+    case Mod(l, r)   => buildExprString("", "Mod", toFormattedString( INDENT)(l), toFormattedString(INDENT)(r))
+    case Var(v)      => v.toString
+    // case Loop(l,r) => buildExprString(prefix)
+    // case Conditional(c,l,r) =>
+    // case Assignment(l,r) =>
+    // case Block(statements: Expr*) => prefix
+    case Loop(l, r)             => buildExprString("",
+      nodeString = "Loop",
+      toFormattedString("" + INDENT)(l),
+      toFormattedString("" + INDENT)(r))
+    case Assignment(l, r)       => buildExprString(l.toString,
+      nodeString = " = ",
+      toFormattedString("" + INDENT)(l),
+      toFormattedString("" + INDENT)(r))
+    case Block(e)               => "" + e.toString //TODO might need to use buildUnaryExpreString
+    case Conditional(e, b1, b2) => buildTrinaryExprString(
+      "",
+      "Conditional",
+      toFormattedString("" + INDENT)(e),
+      toFormattedString("" + INDENT)(b1),
+      toFormattedString("" + INDENT)(b2))
+
   }
 
   def buildExprString(prefix: String, nodeString: String, leftString: String, rightString: String) = {
