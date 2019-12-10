e7427cb64a6b6d6cafa291c3d551acaae858b87b
Edward Hubbard <edhubbard1994@gmail.com>
2019-12-10 14:10:45 -0600

    more tests

--- src/test/scala/TestParsing.scala
+++ src/test/scala/TestParsing.scala
@@ -1,5 +1,10 @@
+import edu.luc.cs.laufer.cs473.expressions.behaviors.Value
 import org.scalatest.FlatSpec
-import edu.luc.cs.laufer.cs473.expressions.CombinatorParser
+import edu.luc.cs.laufer.cs473.expressions.{CombinatorParser, behaviors, _}
+
+import scala.collection.mutable.HashMap
+
+
 
 class TestParsing extends FlatSpec {
 
@@ -21,6 +26,45 @@ class TestParsing extends FlatSpec {
   "A parser " should "evaluate a simple while loop" in {
     var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "while(false){}")
     assert(result.toString.contains("Loop("))
+  }
+  "A parser" should "evaluate a conditional" in {
+    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "if(true){}")
+    assert(result.toString.contains("Conditional("))
+  }
+
+  "A parser" should " evaluate a struct" in {
+    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "{i:10};")
+    assert(result.toString.contains("Struct("))
+    assert(result.toString.contains("(i,Constant(10))"))
+  }
+
+  "A parser " should "evaluate expressions with a hashmap store" in {
+    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "1;")
+    val store = HashMap.empty[String, Value]
+    var res = behaviors.evaluate(store)(result.get)
+    assert(res.toString.contains("Success("))
+  }
+
+  "A Parser" should "not evaluate expressions without a semicolon" in {
+    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "4")
+    val store = HashMap.empty[String, Value]
+    assertThrows[java.lang.RuntimeException]{
+      behaviors.evaluate(store)(result.get)
+    }
+  }
+
+  "A Parser" should "evaluate simple mathematical expressions" in {
+    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "4 + 3;")
+    val store = HashMap.empty[String, Value]
+    var res = behaviors.evaluate(store)(result.get)
+    assert(res.toString.contains("Num(7)"))
+  }
+
+  "A Parser" should "recognize order of operations when evaluating" in {
+    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "1 + 3 * 2 / 1;")
+    val store = HashMap.empty[String, Value]
+    var res = behaviors.evaluate(store)(result.get)
+    assert(res.toString.contains("Num(7)"))
   }
 
 }
