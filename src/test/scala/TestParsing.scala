0ba77ad32f56ac590f6116be72da07b03f99ffce
Edward Hubbard <edhubbard1994@gmail.com>
2019-12-10 13:36:14 -0600

    loop test

--- src/test/scala/TestParsing.scala
+++ src/test/scala/TestParsing.scala
@@ -3,9 +3,9 @@ import edu.luc.cs.laufer.cs473.expressions.CombinatorParser
 
 class TestParsing extends FlatSpec {
 
-  "A parser" should "evaluate a block" in {
-    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "i = 1;")
-    assert(result.toString.contains("Map()"))
+  "A parser" should "evaluate a List" in {
+    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "{}")
+    assert(result.toString.contains("List()"))
   }
 
   "A parser" should "evaluate an assignment" in {
@@ -16,6 +16,11 @@ class TestParsing extends FlatSpec {
   "A parser" should "not evaluate an assignment without a semicolon" in {
     var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "i = 1")
     assert(result.toString.contains("failure: ';' expected but end of source found"))
+  }
+
+  "A parser " should "evaluate a simple while loop" in {
+    var result = CombinatorParser.parseAll(CombinatorParser.topLevel, "while(false){}")
+    assert(result.toString.contains("Loop("))
   }
 
 }
